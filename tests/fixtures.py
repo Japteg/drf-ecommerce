bca2e6b7d78bd4678b8ca9c7e78ffbcefad98fad
japteg <japteg.2698@gmail.com>
2023-04-20 22:28:25 +0530

    part2: added all db models, fixtures and tests

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -1,5 +1,4 @@
 import pytest
-from django.contrib.auth.models import User
 from django.core.management import call_command
 
 
@@ -22,3 +21,12 @@ def db_fixture_setup(django_db_setup, django_db_blocker):
         call_command("loaddata", "db_admin_fixture.json")
         call_command("loaddata", "db_category_fixtures.json")
         call_command("loaddata", "db_product_fixtures.json")
+        call_command("loaddata", "db_type_fixtures.json")
+        call_command("loaddata", "db_brand_fixtures.json")
+        call_command("loaddata", "db_product_inventory_fixtures.json")
+        call_command("loaddata", "db_media_fixtures.json")
+        call_command("loaddata", "db_stock_fixtures.json")
+        call_command("loaddata", "db_product_attribute_fixtures.json")
+        call_command("loaddata", "db_product_attribute_value_fixtures.json")
+        call_command("loaddata", "db_product_attribute_values_fixtures.json")
+        call_command("loaddata", "db_category_product_fixtures.json")
